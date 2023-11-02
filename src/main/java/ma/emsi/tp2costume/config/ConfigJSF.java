1019f85dff8dc2cd6e5cfe5db0caa4b6c34937e4
fz.yech <fatimayechhab@gmail.com>
2023-11-02 10:09:22 +0100

    Exercice1

---
+++ src/main/java/ma/emsi/tp2costume/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tp2costume.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author user
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
